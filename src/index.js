bcd6cc48c10641961f2ff291105abd3105a4add7
gregmoncayo <gregorymoncayo25@hotmail.com>
2020-03-05 14:18:05 -0500

    Added src

--- src/index.js
+++ src/index.js
@@ -19,9 +19,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
-import "assets/vendor/nucleo/css/nucleo.css";
-import "assets/vendor/@fortawesome/fontawesome-free/css/all.min.css";
-import "assets/scss/argon-dashboard-react.scss";
+import "src/assets/vendor/nucleo/css/nucleo.css";
+import "src/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css";
+import "src/assets/scss/argon-dashboard-react.scss";
 
 import AdminLayout from "src/layouts/Admin.jsx";
 import AuthLayout from "src/layouts/Auth.jsx";
